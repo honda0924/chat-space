247d660a060dcdc170faa68ae6bc1d21c51f60f2
honda0924 <honda0924@hotmail.co.jp>
2019-08-25 18:37:10 +0900

    display_sidebar

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,11 @@ class Group < ApplicationRecord
   has_many :groups_users
   has_many :messages
 
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだ投稿がありません'
+    end
+  end
 end

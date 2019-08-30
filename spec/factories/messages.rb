47fd0137ded2986a648afa4b40c0ff11b53a899b
honda0924 <honda0924@hotmail.co.jp>
2019-08-30 22:24:30 +0900

    message_test

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
 
   factory :message do
     body {Faker::Lorem.sentence}
-    image {File.open(#{Rails.root}/public/test.png)}
+    image {File.open("#{Rails.root}/public/test.png")}
     group
     user
   end

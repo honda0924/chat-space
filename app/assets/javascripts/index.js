8e6184172cfaf4c81f4d73a87ba3231e0dafc01f
honda0924 <honda0924@hotmail.co.jp>
2019-09-12 00:34:24 +0900

    add_and_delete_members

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -13,9 +13,19 @@ $(function() {
                 </div>`
     return html;
   }
+  function addUser(userId,userName) {
+    var html = `<div id='chat-group-users'>
+                  <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                    <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                      <p class='chat-group-user__name'>${userName}</p>
+                      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    return html;
+      }
 
   $("#user-search-field").on("keyup",function(){
     var input = $("#user-search-field").val();
+    console.log
 
     $.ajax({
       type: 'GET',
@@ -28,15 +38,27 @@ $(function() {
         $("#user-search-result").empty();
         users.forEach(function(user){
           html = appendUser(user);
+      $("#user-search-result").append(html);
         });
       }else{
         $("#user-search-result").empty();
         html = NoUser("一致するユーザーがいません");
-      }
       $("#user-search-result").append(html);
+      }
     })
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     });
+  });
+  $(document).on("click", ".user-search-add", function(){
+    $('#chat-group-users').val();
+    var userId=$(this).data('user-id');
+    var userName = $(this).data('user-name');
+    html = addUser(userId,userName);
+    $('#chat-group-users').append(html);
+    $(this).parent().remove();
+  });
+  $(document).on("click", ".user-search-remove", function () {
+    $(this).parent().remove();
   });
 });

78c4a90a6ee64c1231000432c1174777a3ff0c3d
honda0924 <honda0924@hotmail.co.jp>
2019-09-22 18:40:52 +0900

    fix_null_keyword_and_arrange_search_view

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,10 +1,8 @@
 $(function() {
   function appendUser(user) {
-    var html = `<div class="chat-group-form__field clearfix>"
-                  <div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user clearfix">
                     <p class="chat-group-user__name">${ user.name }</p>
                     <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id= ${ user.id } data-user-name= ${ user.name }>追加</div>
-                  </div>
                 </div>`
     return html;
   }
